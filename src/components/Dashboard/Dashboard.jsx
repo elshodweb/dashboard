4042742db6a1a0e9205b8ed467af43d90a823e3c
elshod <elshodtukhtamurodov13@gmail.com>
2022-11-29 22:47:52 +0500

    main

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addPath } from "../../store/pathSlice";
-
+import "./Dashboard.scss";
 function Dashboard() {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -9,7 +9,7 @@ function Dashboard() {
   }, [dispatch]);
   return (
     <div className="dashboard">
-      <div className="containeri">dashboard</div>
+      <div className="containeri"></div>
     </div>
   );
 }
